018f0c64df804a0108e8de4375f36aad31a4bcec
questionmarkexclamationpoint <questionmarkexclamation@github.com>
2021-04-11 19:40:43 -0700

    fix Gene scoping

--- lib/evilution/gene.rb
+++ lib/evilution/gene.rb
@@ -1,33 +1,35 @@
-class Gene
-  include Mutable
+module Evilution
+  class Gene
+    include Mutable
 
-  attr_accessor :value, :chance, :rate, :minimum, :maximum
+    attr_accessor :value, :chance, :rate, :minimum, :maximum
 
-  def initialize(value: rand, chance: 0.1, rate: 0.1, minimum: 0.0, maximum: 1.0)
-    @value = value
-    @chance = chance
-    @minimum = minimum
-    @maximum = maximum
-  end
-
-  def mutate!
-    if value.is_a?(Mutable)
-      value.mutate!
-    elsif value.is_a?(Number)
-      @value = mutated_value(value, minimum, maximum, rate, chance)
+    def initialize(value: rand, chance: 0.1, rate: 0.1, minimum: 0.0, maximum: 1.0)
+      @value = value
+      @chance = chance
+      @minimum = minimum
+      @maximum = maximum
     end
 
-    self
-  end
+    def mutate!
+      if value.is_a?(Mutable)
+        value.mutate!
+      elsif value.is_a?(Number)
+        @value = mutated_value(value, minimum, maximum, rate, chance)
+      end
 
-  def recombine(other)
-    child = self.reproduce
-    child.value = other.value if rand < 0.5
+      self
+    end
 
-    child
-  end
+    def recombine(other)
+      child = self.reproduce
+      child.value = other.value if rand < 0.5
 
-  def reproduce
-    Marshal.load(Marshal.dump(self))
+      child
+    end
+
+    def reproduce
+      Marshal.load(Marshal.dump(self))
+    end
   end
 end
